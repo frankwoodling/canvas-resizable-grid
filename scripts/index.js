75f8ee8eb8183d7e608b9215a1783f76f2fe39ce
Frank Woodling <fwoodling@gmail.com>
2018-11-21 01:14:33 -0500

    grid is now created at a specified size
    This can still be made responsive by adding an event listener and redrawing the grid on a window resize.

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,38 @@
-function getSquare(canvas, evt) {
-  let rect = canvas.getBoundingClientRect();
-  return {
-    x: 1 + (evt.clientX - rect.left) - (evt.clientX - rect.left)%10,
-    y: 1 + (evt.clientY - rect.top) - (evt.clientY - rect.top)%10
-  };
-}
+const gridWidth = 1200,
+      gridHeight = 400,
+      gridSize = 25;
 
-let zoomLevel = 15;
+function drawGrid(context, width, height, size = 1) {
 
-function drawGrid(context) {
-
-  for (let x = 0; x < context.canvas.width; x += zoomLevel) {
+  for (let x = 0.5; x < width + 1; x += size) {
     context.moveTo(x, 0);
-    context.lineTo(x, context.canvas.width);
+    context.lineTo(x, height);
   }
 
-  for (let y = 0; y < context.canvas.width; y += zoomLevel) {
+  for (let y = 0.5; y < height + 1; y += size) {
     context.moveTo(0, y);
-    context.lineTo(context.canvas.width*zoomLevel, y);
+    context.lineTo(width, y);
   }
 
   context.strokeStyle = "#ddd";
   context.stroke();
-
-}
-
-function fillSquare(context, x, y){
-  context.fillStyle = "gray";
-  context.fillRect(Math.ceil(zoomLevel/x)*zoomLevel,y,Math.floor(zoomLevel/y)*zoomLevel, zoomLevel);
 }
 
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 
-drawGrid(context);
+canvas.setAttribute('width', (gridWidth + 1).toString());
+canvas.setAttribute('height', (gridHeight + 1).toString());
 
-canvas.addEventListener('click', handleClick);
+drawGrid(context, gridWidth, gridHeight, gridSize);
 
-function handleClick(e) {
+canvas.addEventListener('click', handleFillBox);
+
+function handleFillBox(e) {
   context.fillStyle = "black";
-
-  context.fillRect(Math.floor(e.offsetX / zoomLevel) * zoomLevel,
-    Math.floor(e.offsetY / zoomLevel) * zoomLevel,
-    zoomLevel,
-    zoomLevel);
+  context.fillRect(
+    Math.floor(e.offsetX / gridSize) * gridSize,
+    Math.floor(e.offsetY / gridSize) * gridSize,
+    gridSize,
+    gridSize);
 }
-
-canvas.addEventListener('click', function(evt) {
-  let mousePos = getSquare(canvas, evt);
-  fillSquare(context, mousePos.x, mousePos.y)
-}, false);
-
-function handleResize() {
-
-  function handleResize() {
-    let w = window.innerWidth,
-        h = window.innerHeight;
-
-    context.canvas.width = w;
-    context.canvas.height = h;
-
-    let windowRatio = w/h;
-    let scale = w/100;
-    if (windowRatio > 100) {
-        scale = h/100;
-    }
-  }
-
-  drawGrid(context)
-}
-
-window.addEventListener("resize", handleResize);
-
-// https://stackoverflow.com/questions/13990128/how-to-fill-a-cell-on-clicking-the-grid-on-canvas-in-html5
