3222a9201d5316fabc7d9b13aa0fb06edea0ddd7
Frank Woodling <fwoodling@gmail.com>
2018-11-25 20:43:14 -0500

    created class for grid functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,88 @@
-const gridWidth = 500,
-      gridHeight = 500,
-      gridSize = 50;
+class Grid {
 
-const drawGrid = (context, width, height, size = 1) => {
-
-  for (let x = 0.5; x < width + 1; x += size) {
-    context.moveTo(x, 0);
-    context.lineTo(x, height);
+  constructor(width, height, size, color) {
+    this.gridWidth = width;
+    this.gridHeight = height;
+    this.gridSize = size;
+    this.gridColor = color;
+    this.boxColorTrue = 'black';
+    this.boxColorFalse = 'white';
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.addEventListener('click', this.handleGridClick.bind(this));
   }
 
-  for (let y = 0.5; y < height + 1; y += size) {
-    context.moveTo(0, y);
-    context.lineTo(width, y);
+  setupCanvas() {
+    this.canvas.setAttribute('width', (this.gridWidth + 1).toString());
+    this.canvas.setAttribute('height', (this.gridHeight + 1).toString());
   }
 
-  context.strokeStyle = "#ddd";
-  context.stroke();
-};
+  drawGrid() {
+    for (let x = 0.5; x < this.gridWidth + 1; x += this.gridSize) {
+      this.ctx.moveTo(x, 0);
+      this.ctx.lineTo(x, this.gridHeight);
+    }
 
-const initArr = (n, m, fill) => {
-  return Array(m).fill().map(() => Array(n).fill(fill));
-};
+    for (let y = 0.5; y < this.gridHeight + 1; y += this.gridSize) {
+      this.ctx.moveTo(0, y);
+      this.ctx.lineTo(this.gridWidth, y);
+    }
 
-let gridArr = initArr(gridWidth/gridSize, gridHeight/gridSize, false);
+    this.ctx.strokeStyle = this.gridColor;
+    this.ctx.stroke();
+  }
 
-let handleFillBox = (e) => {
-  let colorTrue = 'black',
-      colorFalse = 'white',
-      xPos =  Math.floor(e.offsetX / gridSize),
-      yPos =  Math.floor(e.offsetY / gridSize);
+  fillBox(x, y, size, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(
+      Math.floor(x) * size,
+      Math.floor(y) * size,
+      size,
+      size);
+  }
 
-  gridArr[yPos][xPos] = !gridArr[yPos][xPos];
+  handleGridClick(e) {
+    let colorTrue = this.boxColorTrue,
+        colorFalse = this.boxColorFalse,
+        xPos =  Math.floor(e.offsetX / this.gridSize),
+        yPos =  Math.floor(e.offsetY / this.gridSize);
 
-  if (gridArr[yPos][xPos]) fillBox(xPos, yPos, gridSize, colorTrue);
-  else fillBox(xPos, yPos, gridSize, colorFalse);
+    this.gridArr[yPos][xPos] = !this.gridArr[yPos][xPos];
 
-  // This works but erases grid.  Should I redraw entire grid on each click or only the specific section?
-  // How can the if else statement be improved?  Could make it ternary.  It also looks up the array element a second time.
-};
+    let colorChoose = grid.gridArr[yPos][xPos] ? grid.fillBox(xPos, yPos, grid.gridSize, colorTrue) :
+      grid.fillBox(xPos, yPos, grid.gridSize, colorFalse);
 
-const fillBox = (x, y, size, color) => {
-  context.fillStyle = color;
-  context.fillRect(
-    Math.floor(x) * size,
-    Math.floor(y) * size,
-    size,
-    size);
-};
+    grid.drawGrid();
+  };
 
+  fillGridArr(n, m, fill) {
+    return Array(m).fill().map(() => Array(n).fill(fill));
+  }
+
+  initGridArr() {
+    this.gridArr = this.fillGridArr(this.gridWidth/this.gridSize, this.gridHeight/this.gridSize, false);
+  }
+}
+
+let grid = new Grid(700, 400, 50, '#ddd');
+grid.setupCanvas();
+grid.initGridArr();
+grid.drawGrid();
+
+// TODO: For debugging only.  Remove when finished.
 const getMouseBoxPos = (e) => {
   return { x:e.clientX, y:e.clientY };
 };
 
 (styleCoordText => {
-  document.getElementById("mouseDiv").style.width = gridWidth + 'px';
+  document.getElementById("mouseDiv").style.width = grid.gridWidth + 'px';
   document.getElementById("mouseDiv").style.textAlign = 'center';
 })();
 
-const canvas = document.getElementById('canvas'),
-      context = canvas.getContext('2d');
-
-canvas.setAttribute('width', (gridWidth + 1).toString());
-canvas.setAttribute('height', (gridHeight + 1).toString());
-
-drawGrid(context, gridWidth, gridHeight, gridSize);
-
-canvas.addEventListener('click', handleFillBox);
-
 document.onmousemove = (e) => {
   let mouseBoxPos = getMouseBoxPos(e),
-      mouseBoxPosX = Math.floor(mouseBoxPos.x / gridSize);
-      mouseBoxPosY = Math.floor(mouseBoxPos.y / gridSize);
+    mouseBoxPosX = Math.floor(mouseBoxPos.x / grid.gridSize);
+  mouseBoxPosY = Math.floor(mouseBoxPos.y / grid.gridSize);
 
   mouseDiv.textContent = 'x: ' + mouseBoxPosX + ', y: ' + mouseBoxPosY;
 };
